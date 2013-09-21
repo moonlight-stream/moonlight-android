00fca9462c0459c33c1aed2bedeabd8a67de0177
Cameron Gutman <cameron.gutman@case.edu>
2013-09-21 01:04:32 -0400

    Eclipse autogenerated project files

---
+++ src/com/limelight/Connection.java
@@ -0,0 +1,14 @@
+package com.limelight;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Connection extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_connection);
+	}
+
+}

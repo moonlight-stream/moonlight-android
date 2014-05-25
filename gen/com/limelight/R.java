e83bc747c8953d9bbc2edfbe929aba2e208e57af
Cameron Gutman <aicommander@gmail.com>
2014-05-24 20:28:38 -0400

    Add assets and manifest updates for OUYA store

--- gen/com/limelight/R.java
+++ gen/com/limelight/R.java
@@ -32,7 +32,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int app_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ouya_icon=0x7f020002;
     }
     public static final class id {
         public static final int autoDec=0x7f080006;

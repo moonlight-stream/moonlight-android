07cf96c5ceadf6ded93ef51882701352f0681e3f
Cameron Gutman <aicommander@gmail.com>
2014-06-15 04:40:47 -0700

    Add support for secure pairing and input encryption to fix GFE 2.1 compatibility.
    TODO:
    Needs a LimelightCryptoProvider implementation for each platform to work.
    Untested (and probably broken) on Android.
    Needs more testing in general, especially in corner cases.

---
+++ moonlight-common/src/com/limelight/nvstream/http/LimelightCryptoProvider.java
@@ -0,0 +1,9 @@
+package com.limelight.nvstream.http;
+
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+
+public interface LimelightCryptoProvider {
+	public X509Certificate getClientCertificate();
+	public RSAPrivateKey getClientPrivateKey();
+}
--- moonlight-common/src/com/limelight/nvstream/input/NvController.java
+++ moonlight-common/src/com/limelight/nvstream/input/NvController.java
@@ -5,6 +5,15 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 public class NvController {
 	
@@ -15,10 +24,27 @@ public class NvController {
 	private InetAddress host;
 	private Socket s;
 	private OutputStream out;
+	private Cipher riCipher;
+
 	
-	public NvController(InetAddress host)
+	private final static byte[] ENCRYPTED_HEADER = new byte[] {0x00, 0x00, 0x00, 0x20};
+	
+	public NvController(InetAddress host, SecretKey riKey)
 	{
 		this.host = host;
+		try {
+			// This cipher is guaranteed to be supported
+			this.riCipher = Cipher.getInstance("AES/CBC/NoPadding");
+			this.riCipher.init(Cipher.ENCRYPT_MODE, riKey, new IvParameterSpec(new byte[16]));
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (InvalidAlgorithmParameterException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void initialize() throws IOException
@@ -36,36 +62,51 @@ public class NvController {
 		} catch (IOException e) {}
 	}
 	
+	private byte[] encryptAesInputData(byte[] data) throws Exception {
+		// Input data is rounded to units of 32 bytes
+		byte[] blockRoundedData = Arrays.copyOf(data, 32);
+		return riCipher.update(blockRoundedData);
+	}
+	
+	private void sendPacket(InputPacket packet) throws IOException {
+		out.write(ENCRYPTED_HEADER);
+		byte[] encryptedInput;
+		try {
+			encryptedInput = encryptAesInputData(packet.toWire());
+		} catch (Exception e) {
+			// Should never happen
+			e.printStackTrace();
+			return;
+		}
+		out.write(encryptedInput);
+		out.flush();
+	}
+	
 	public void sendControllerInput(short buttonFlags, byte leftTrigger, byte rightTrigger,
 			short leftStickX, short leftStickY, short rightStickX, short rightStickY) throws IOException
 	{
-		out.write(new ControllerPacket(buttonFlags, leftTrigger,
+		sendPacket(new ControllerPacket(buttonFlags, leftTrigger,
 				rightTrigger, leftStickX, leftStickY,
-				rightStickX, rightStickY).toWire());
-		out.flush();
+				rightStickX, rightStickY));
 	}
 	
 	public void sendMouseButtonDown(byte mouseButton) throws IOException
 	{
-		out.write(new MouseButtonPacket(true, mouseButton).toWire());
-		out.flush();
+		sendPacket(new MouseButtonPacket(true, mouseButton));
 	}
 	
 	public void sendMouseButtonUp(byte mouseButton) throws IOException
 	{
-		out.write(new MouseButtonPacket(false, mouseButton).toWire());
-		out.flush();
+		sendPacket(new MouseButtonPacket(false, mouseButton));
 	}
 	
 	public void sendMouseMove(short deltaX, short deltaY) throws IOException
 	{
-		out.write(new MouseMovePacket(deltaX, deltaY).toWire());
-		out.flush();
+		sendPacket(new MouseMovePacket(deltaX, deltaY));
 	}
 	
 	public void sendKeyboardInput(short keyMap, byte keyDirection, byte modifier) throws IOException 
 	{
-		out.write(new KeyboardPacket(keyMap, keyDirection, modifier).toWire());
-		out.flush();
+		sendPacket(new KeyboardPacket(keyMap, keyDirection, modifier));
 	}
 }

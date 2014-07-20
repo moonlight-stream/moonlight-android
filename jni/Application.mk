6a7e06b3d55bf8a3b74f891f178bfec60829b2d8
Cameron Gutman <aicommander@gmail.com>
2014-07-20 14:19:08 -0700

    Rebuild native libraries with GCC 4.9 and NDK 10. Added native libraries for ARMv8 and x86_64.

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,8 +3,7 @@
 # Our minimum version is Android 4.1
 APP_PLATFORM := android-16
 
-# NOTE: our armeabi-v7a libraries require NEON support
-APP_ABI := armeabi-v7a x86 mips
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64 mips #mips64
 
 # We want an optimized build
 APP_OPTIM := release

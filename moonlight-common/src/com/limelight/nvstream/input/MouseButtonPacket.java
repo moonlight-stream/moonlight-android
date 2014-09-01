e7f1b822f7860d6d4306d69a4b65d3972c639b55
Cameron Gutman <aicommander@gmail.com>
2014-09-01 14:02:53 -0700

    Rename button constants to something reasonable

--- moonlight-common/src/com/limelight/nvstream/input/MouseButtonPacket.java
+++ moonlight-common/src/com/limelight/nvstream/input/MouseButtonPacket.java
@@ -16,12 +16,9 @@ public class MouseButtonPacket extends InputPacket {
 	public static final byte PRESS_EVENT = 0x07;
 	public static final byte RELEASE_EVENT = 0x08;
 	
-	// left
-	public static final byte BUTTON_1 = 0x01;
-	// middle
-	public static final byte BUTTON_2 = 0x02;
-	// right
-	public static final byte BUTTON_3 = 0x03;
+	public static final byte BUTTON_LEFT = 0x01;
+	public static final byte BUTTON_MIDDLE = 0x02;
+	public static final byte BUTTON_RIGHT = 0x03;
 	
 	public MouseButtonPacket(boolean buttonDown, byte mouseButton)
 	{

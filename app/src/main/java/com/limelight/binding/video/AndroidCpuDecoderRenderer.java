afbe64f3ff1dd1ac71c4670fe847953cc261ad2b
Cameron Gutman <aicommander@gmail.com>
2014-11-16 17:19:07 -0800

    Remove an unused import

--- app/src/main/java/com/limelight/binding/video/AndroidCpuDecoderRenderer.java
+++ app/src/main/java/com/limelight/binding/video/AndroidCpuDecoderRenderer.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.concurrent.locks.LockSupport;
 
 import android.graphics.PixelFormat;
 import android.os.Build;

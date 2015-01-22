79ddfc65d5ffa565961d4d6b5777ddb491b18403
Cameron Gutman <aicommander@gmail.com>
2015-01-22 14:17:33 -0500

    Remove unused import

--- moonlight-common/src/com/limelight/nvstream/av/video/VideoStream.java
+++ moonlight-common/src/com/limelight/nvstream/av/video/VideoStream.java
@@ -1,7 +1,6 @@
 package com.limelight.nvstream.av.video;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;

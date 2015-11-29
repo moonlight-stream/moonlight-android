365ba6f380f3ac5c2e9bac33b96b15ddeccc0fdc
Anni Wang <annikwang.ca@gmail.com>
2015-11-29 14:49:11 -0800

    second try to revert back to original

--- app/src/main/java/com/limelight/nvstream/mdns/MdnsDiscoveryListener.java
+++
@@ -1,7 +0,0 @@
-package com.limelight.nvstream.mdns;
-
-public interface MdnsDiscoveryListener {
-	public void notifyComputerAdded(MdnsComputer computer);
-	public void notifyComputerRemoved(MdnsComputer computer);
-	public void notifyDiscoveryFailure(Exception e);
-}

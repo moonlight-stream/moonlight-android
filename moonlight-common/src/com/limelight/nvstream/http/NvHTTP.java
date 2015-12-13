5112179fca94a488423535ed7729e531ba753896
Cameron Gutman <aicommander@gmail.com>
2015-12-13 12:18:42 -0800

    Check for GFE version attribute before enabling 4K

--- moonlight-common/src/com/limelight/nvstream/http/NvHTTP.java
+++ moonlight-common/src/com/limelight/nvstream/http/NvHTTP.java
@@ -374,6 +374,16 @@ public class NvHTTP {
 			return false;
 		}
 		
+		// GFE 2.7 released without 4K support, even though it claims to have such
+		// support using the SupportedDisplayMode element. We'll use the new GfeVersion
+		// element to tell whether 4K is supported or not. For now, we just check the existence
+		// of this element. I'm hopeful that the production version that ships with 4K will
+		// also be the first production version that has this element.
+		String gfeVersion = getXmlString(serverInfo, "GfeVersion");
+		if (gfeVersion == null) {
+			return false;
+		}
+		
 		try {
 			if (Integer.parseInt(heightStr) >= 2160) {
 				// Found a 4K resolution in the list

ac8b7ae9605569949f394aa01d370b880e2534a4
Cameron Gutman <aicommander@gmail.com>
2017-05-14 20:39:39 -0700

    Add OpenSSL and mk files for moonlight-core

---
+++ moonlight-common/src/main/jni/moonlight-core/openssl/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libssl
+LOCAL_SRC_FILES:= $(TARGET_ARCH_ABI)/libssl.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_STATIC_LIBRARY)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libcrypto
+LOCAL_SRC_FILES:= $(TARGET_ARCH_ABI)/libcrypto.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_STATIC_LIBRARY)

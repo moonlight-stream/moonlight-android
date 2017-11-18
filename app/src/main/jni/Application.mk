b774b472132d74acb5dba8a0dc9986b59988b18a
Cameron Gutman <aicommander@gmail.com>
2017-11-18 13:38:45 -0800

    Update for NDK 16 (deprecating MIPS)

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,10 +1,4 @@
-# Application.mk for Limelight
+# Application.mk for Moonlight
 
 # Our minimum version is Android 4.1
 APP_PLATFORM := android-16
-
-# Support all modern ABIs
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64 mips mips64
-
-# We want an optimized build
-APP_OPTIM := release

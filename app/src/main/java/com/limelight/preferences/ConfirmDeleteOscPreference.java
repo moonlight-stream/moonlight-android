9d7a28e408ee4203e50dbdddd9d7371543c1ca1a
Cameron Gutman <aicommander@gmail.com>
2018-02-25 13:33:52 -0800

    Implement deletion of OSC settings

---
+++ app/src/main/java/com/limelight/preferences/ConfirmDeleteOscPreference.java
@@ -0,0 +1,41 @@
+package com.limelight.preferences;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.widget.Toast;
+
+import com.limelight.R;
+
+import static com.limelight.binding.input.virtual_controller.VirtualControllerConfigurationLoader.OSC_PREFERENCE;
+
+public class ConfirmDeleteOscPreference extends DialogPreference {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ConfirmDeleteOscPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public ConfirmDeleteOscPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ConfirmDeleteOscPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ConfirmDeleteOscPreference(Context context) {
+        super(context);
+    }
+
+    public void onClick(DialogInterface dialog, int which) {
+        if (which == DialogInterface.BUTTON_POSITIVE) {
+            getContext().getSharedPreferences(OSC_PREFERENCE, Context.MODE_PRIVATE).edit().clear().apply();
+            Toast.makeText(getContext(), R.string.toast_reset_osc_success, Toast.LENGTH_SHORT).show();
+        }
+    }
+}

16b845ab84fbad8a6fc3a7c4b3c1ff0f875bc6b6
Cameron Gutman <aicommander@gmail.com>
2018-12-19 15:06:46 +0500

    Hide the mouse cursor during pointer capture to work around DeX bug

--- app/src/main/java/com/limelight/binding/input/capture/InputCaptureManager.java
+++ app/src/main/java/com/limelight/binding/input/capture/InputCaptureManager.java
@@ -12,7 +12,7 @@ public class InputCaptureManager {
     public static InputCaptureProvider getInputCaptureProvider(Activity activity, EvdevListener rootListener) {
         if (AndroidNativePointerCaptureProvider.isCaptureProviderSupported()) {
             LimeLog.info("Using Android O+ native mouse capture");
-            return new AndroidNativePointerCaptureProvider(activity.findViewById(R.id.surfaceView));
+            return new AndroidNativePointerCaptureProvider(activity, activity.findViewById(R.id.surfaceView));
         }
         // LineageOS implemented broken NVIDIA capture extensions, so avoid using them on root builds.
         // See https://github.com/LineageOS/android_frameworks_base/commit/d304f478a023430f4712dbdc3ee69d9ad02cebd3

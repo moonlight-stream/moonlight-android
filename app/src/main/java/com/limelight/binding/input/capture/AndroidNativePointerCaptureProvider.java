81582d734386d2209074f87ad153bad885be3d17
Cameron Gutman <aicommander@gmail.com>
2018-12-24 19:58:19 -0800

    Revert "Hide the mouse cursor during pointer capture to work around DeX bug"
    It doesn't actually fix the bug.
    
    This reverts commit 16b845ab84fbad8a6fc3a7c4b3c1ff0f875bc6b6.

--- app/src/main/java/com/limelight/binding/input/capture/AndroidNativePointerCaptureProvider.java
+++ app/src/main/java/com/limelight/binding/input/capture/AndroidNativePointerCaptureProvider.java
@@ -1,46 +1,27 @@
 package com.limelight.binding.input.capture;
 
 import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Context;
 import android.os.Build;
 import android.view.InputDevice;
 import android.view.MotionEvent;
-import android.view.PointerIcon;
 import android.view.View;
-import android.view.ViewGroup;
 
 @TargetApi(Build.VERSION_CODES.O)
 public class AndroidNativePointerCaptureProvider extends InputCaptureProvider {
-    private Context context;
-    private View targetView;
-    private ViewGroup rootViewGroup;
 
-    public AndroidNativePointerCaptureProvider(Activity activity, View targetView) {
-        this.context = activity;
+    private View targetView;
+
+    public AndroidNativePointerCaptureProvider(View targetView) {
         this.targetView = targetView;
-        this.rootViewGroup = (ViewGroup) activity.getWindow().getDecorView();
     }
 
     public static boolean isCaptureProviderSupported() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
     }
 
-    // DeX on Android 8.1 doesn't properly hide the mouse pointer when running in
-    // windowed mode, even though the cursor is properly captured (and thus doesn't move anymore).
-    // To work around this issue, hide the pointer icon when requesting pointer capture.
-    private void setPointerIconOnAllViews(PointerIcon icon) {
-        for (int i = 0; i < rootViewGroup.getChildCount(); i++) {
-            View view = rootViewGroup.getChildAt(i);
-            view.setPointerIcon(icon);
-        }
-        rootViewGroup.setPointerIcon(icon);
-    }
-
     @Override
     public void enableCapture() {
         super.enableCapture();
-        setPointerIconOnAllViews(PointerIcon.getSystemIcon(context, PointerIcon.TYPE_NULL));
         targetView.requestPointerCapture();
     }
 
@@ -48,7 +29,6 @@ public class AndroidNativePointerCaptureProvider extends InputCaptureProvider {
     public void disableCapture() {
         super.disableCapture();
         targetView.releasePointerCapture();
-        setPointerIconOnAllViews(null);
     }
 
     @Override

421567119118f2892f9e9051d7373a7aac40be7f
ViktorPih <793979901@qq.com>
2021-03-19 19:38:55 +0800

    # 增加自定义实时程序模式

--- app/src/main/jni/moonlight-core/VideoDecoder/VideoStats.c
+++ app/src/main/jni/moonlight-core/VideoDecoder/VideoStats.c
@@ -6,36 +6,36 @@
 #include <sys/time.h>
 #include <assert.h>
 
-uint64_t getTimeMsec()
+int64_t getTimeMsec()
 {
     struct timeval t;
     gettimeofday(&t, 0);
-    return (uint64_t)((uint64_t)t.tv_sec * 1000 + t.tv_usec/1000);
+    return (int64_t)((int64_t)t.tv_sec * 1000 + t.tv_usec/1000);
 //    return getClockMsec();
 }
 
-uint64_t getTimeUsec(void) {
+int64_t getTimeUsec(void) {
 
     struct timeval t;
     gettimeofday(&t, 0);
-    return (uint64_t)((uint64_t)t.tv_sec * 1000*1000 + t.tv_usec);
+    return (int64_t)((int64_t)t.tv_sec * 1000*1000 + t.tv_usec);
 //    return getClockUsec();
 }
 
-uint64_t getTimeNanc(void) {
+int64_t getTimeNanc(void) {
 
     struct timeval t;
     gettimeofday(&t, 0);
-    return (uint64_t)((uint64_t)t.tv_sec * 1000*1000*1000 + t.tv_usec*1000);
+    return (int64_t)((int64_t)t.tv_sec * 1000*1000*1000 + t.tv_usec*1000);
 }
 
-uint64_t getClockMsec(void) {
+int64_t getClockMsec(void) {
     struct timespec tv;
     clock_gettime(CLOCK_MONOTONIC, &tv);
     return (tv.tv_sec * 1000) + (tv.tv_nsec / 1000 / 1000);
 }
 
-uint64_t getClockUsec(void) {
+int64_t getClockUsec(void) {
 //#if defined(LC_WINDOWS)
 //    return GetTickCount64();
 //#elif HAVE_CLOCK_GETTIME
@@ -56,7 +56,7 @@ uint64_t getClockUsec(void) {
     return (tv.tv_sec * 1000 * 1000) + (tv.tv_nsec / 1000);
 }
 
-uint64_t getClockNanc(void) {
+int64_t getClockNanc(void) {
     struct timespec tv;
     clock_gettime(CLOCK_MONOTONIC, &tv);
     return (tv.tv_sec * 1000 * 1000 * 1000) + (tv.tv_nsec);

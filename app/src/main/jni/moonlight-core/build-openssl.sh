f87806b1b41de9206c5fb2fb1c63e91b21e6198f
Cameron Gutman <aicommander@gmail.com>
2021-04-18 18:23:56 -0500

    Update to OpenSSL 1.1.1k without no-asm

---
+++ app/src/main/jni/moonlight-core/build-openssl.sh
@@ -0,0 +1,27 @@
+PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+OUTPUT_DIR=~/openssl
+
+BASE_ARGS=no-shared no-ssl3 no-stdio no-engine no-hw
+
+set -e
+
+./Configure android-arm $BASE_ARGS -D__ANDROID_API__=16
+make clean
+make build_libs -j`nproc`
+cp lib*.a $OUTPUT_DIR/armeabi-v7a/
+
+./Configure android-arm64 $BASE_ARGS -D__ANDROID_API__=21
+make clean
+make build_libs -j`nproc`
+cp lib*.a $OUTPUT_DIR/arm64-v8a/
+
+./Configure android-x86 $BASE_ARGS -D__ANDROID_API__=16
+make clean
+make build_libs -j`nproc`
+cp lib*.a $OUTPUT_DIR/x86/
+
+./Configure android-x86_64 $BASE_ARGS -D__ANDROID_API__=21
+make clean
+make build_libs -j`nproc`
+cp lib*.a $OUTPUT_DIR/x86_64/
+cp -R include/ $OUTPUT_DIR/include

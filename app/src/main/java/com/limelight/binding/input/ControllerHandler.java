bfc3116661d123ca75842527233c264cf5aaa458
peerobo <phuongrobotics@hotmail.com>
2022-07-20 20:37:32 +0700

    Add support for 8bitdo sn30 pro xCloud (#1102)
    * add support for 8bitdo sn30 pro xCloud
    
    Co-authored-by: peerobo <phuongrobotics@me.com>

--- app/src/main/java/com/limelight/binding/input/ControllerHandler.java
+++ app/src/main/java/com/limelight/binding/input/ControllerHandler.java
@@ -520,6 +520,9 @@ public class ControllerHandler implements InputManager.InputDeviceListener, UsbD
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             context.vendorId = dev.getVendorId();
             context.productId = dev.getProductId();
+            if (context.vendorId == 0x2dc8 && context.productId == 0x2100) {
+                context.is8BitdoSn30Xcloud = true;
+            }
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && hasDualAmplitudeControlledRumbleVibrators(dev.getVibratorManager())) {
@@ -568,6 +571,16 @@ public class ControllerHandler implements InputManager.InputDeviceListener, UsbD
             // Others use THROTTLE and BRAKE (like Xiaomi)
             context.leftTriggerAxis = MotionEvent.AXIS_BRAKE;
             context.rightTriggerAxis = MotionEvent.AXIS_THROTTLE;
+        }
+        else if (context.is8BitdoSn30Xcloud) 
+        {
+            // Specific configs for 8BitDo SN30 Pro for Xbox
+            context.leftTriggerAxis = MotionEvent.AXIS_RX;
+            context.rightTriggerAxis = MotionEvent.AXIS_RY;
+            context.triggersIdleNegative = false;
+            context.triggerDeadzone = 0.30f;
+            context.hasSelect = true;
+            context.hasMode = true;
         }
         else
         {
@@ -928,6 +941,14 @@ public class ControllerHandler implements InputManager.InputDeviceListener, UsbD
         // Override mode button for 8BitDo controllers
         if (context.vendorId == 0x2dc8 && event.getScanCode() == 306) {
             return KeyEvent.KEYCODE_BUTTON_MODE;
+        }
+        // Override select button for 8BitDo SN30 Pro for Xbox
+        if (context.is8BitdoSn30Xcloud) {
+            switch(event.getScanCode()) {
+                case 312:
+                    return KeyEvent.KEYCODE_BUTTON_SELECT;
+                default:
+            }
         }
 
         // This mapping was adding in Android 10, then changed based on
@@ -1960,6 +1981,7 @@ public class ControllerHandler implements InputManager.InputDeviceListener, UsbD
         public boolean hatXAxisUsed, hatYAxisUsed;
 
         public boolean isNonStandardDualShock4;
+        public boolean is8BitdoSn30Xcloud;
         public boolean usesLinuxGamepadStandardFaceButtons;
         public boolean isNonStandardXboxBtController;
         public boolean isServal;

86ff96c9f966af84ac33b93e65b92b899c2bd283
deniz yılmaz <deniz_ylmz_in@windowslive.com>
2022-09-24 06:37:00 +0300

    Physical keyboard accessibility service for preventing default shortcut actions (it's separated by product flavor). Full translation at Turkish language.

---
+++ app/src/nonRootAccessibility/java/com/limelight/KeyboardAccessibilityService.java
@@ -0,0 +1,62 @@
+package com.limelight;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.view.KeyEvent;
+import android.view.accessibility.AccessibilityEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KeyboardAccessibilityService extends AccessibilityService {
+
+    private final static List<int> BLACKLISTED_KEYS = Arrays.asList(
+            KeyEvent.KEYCODE_VOLUME_UP,
+            KeyEvent.KEYCODE_VOLUME_DOWN,
+            KeyEvent.KEYCODE_POWER
+    );
+
+    @Override
+    public boolean onKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+
+        if (Game.instance != null && Game.instance.connected && !BLACKLISTED_KEYS.contains(keyCode)) {
+
+            //Preventing default will disable shortcut actions like alt+tab and etc.
+
+            if (action == KeyEvent.ACTION_DOWN) {
+                Game.instance.handleKeyDown(event);
+                return true;
+            } else if (action == KeyEvent.ACTION_UP) {
+                Game.instance.handleKeyUp(event);
+                return true;
+            }
+        }
+
+        return super.onKeyEvent(event);
+    }
+
+    @Override
+    public void onServiceConnected() {
+        LimeLog.info("Keyboard service is connected");
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.packageNames = new String[] { BuildConfig.APPLICATION_ID };
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.notificationTimeout = 100;
+        info.flags = AccessibilityServiceInfo.FLAG_REQUEST_FILTER_KEY_EVENTS;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+        setServiceInfo(info);
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}

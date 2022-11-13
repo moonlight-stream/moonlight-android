ed600805961aa8796fbd362e0296f8820054736f
luten145 <61258117+luten145@users.noreply.github.com>
2022-11-13 12:53:25 +0900

    Added MetaKey(WindowKey) Packet
    Allows you to use Windows key combinations.
    
    ex) Win+Tab , Win+D

--- app/src/main/java/com/limelight/nvstream/input/KeyboardPacket.java
+++ app/src/main/java/com/limelight/nvstream/input/KeyboardPacket.java
@@ -7,4 +7,5 @@ public class KeyboardPacket {
     public static final byte MODIFIER_SHIFT = 0x01;
     public static final byte MODIFIER_CTRL = 0x02;
     public static final byte MODIFIER_ALT = 0x04;
+    public static final byte MODIFIER_META = 0x08;
 }

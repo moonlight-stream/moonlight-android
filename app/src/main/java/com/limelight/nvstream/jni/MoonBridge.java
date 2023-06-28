803ad116fbed2d701f35ee31a2bb6d30a10cbf3c
Cameron Gutman <aicommander@gmail.com>
2023-06-28 17:26:11 -0500

    Pull in latest moonlight-common-c change

--- app/src/main/java/com/limelight/nvstream/jni/MoonBridge.java
+++ app/src/main/java/com/limelight/nvstream/jni/MoonBridge.java
@@ -85,6 +85,7 @@ public class MoonBridge {
     public static final byte LI_TOUCH_EVENT_BUTTON_ONLY = 0x05;
     public static final byte LI_TOUCH_EVENT_HOVER_LEAVE = 0x06;
 
+    public static final byte LI_TOOL_TYPE_UNKNOWN = 0x00;
     public static final byte LI_TOOL_TYPE_PEN = 0x01;
     public static final byte LI_TOOL_TYPE_ERASER = 0x02;
 

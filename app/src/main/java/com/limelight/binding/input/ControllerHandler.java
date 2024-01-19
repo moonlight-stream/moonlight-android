a08f15a5c10070fbcd22e1271e85872f7bab0976
Alek Lefebvre <alek.lefebvre@gmail.com>
2024-01-18 22:04:47 -0500

    fix loss of precision in rumble strength

--- app/src/main/java/com/limelight/binding/input/ControllerHandler.java
+++ app/src/main/java/com/limelight/binding/input/ControllerHandler.java
@@ -2108,10 +2108,10 @@ public class ControllerHandler implements InputManager.InputDeviceListener, UsbD
                 // We cast the unsigned short value to a signed int before multiplying by
                 // the preferred strength. The resulting value is capped at 65534 before
                 // we cast it back to a short so it doesn't go above 100%.
-                short lowFreqMotorAdjusted = (short)(Math.min(((lowFreqMotor & 0xffff)
-                        /100*prefConfig.vibrateFallbackToDeviceStrength), Short.MAX_VALUE*2));
-                short highFreqMotorAdjusted = (short)(Math.min(((highFreqMotor & 0xffff)
-                        /100*prefConfig.vibrateFallbackToDeviceStrength), Short.MAX_VALUE*2));
+                short lowFreqMotorAdjusted = (short)(Math.min((((lowFreqMotor & 0xffff)
+                        * prefConfig.vibrateFallbackToDeviceStrength) / 100), Short.MAX_VALUE*2));
+                short highFreqMotorAdjusted = (short)(Math.min((((highFreqMotor & 0xffff)
+                        * prefConfig.vibrateFallbackToDeviceStrength) / 100), Short.MAX_VALUE*2));
 
                 rumbleSingleVibrator(deviceVibrator, lowFreqMotorAdjusted, highFreqMotorAdjusted);
             }
